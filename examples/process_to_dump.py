85b58abe9788915993729cc45048778e50f3671d
AuthorName <Author@email.com>
2025-09-13 17:25:09 +0100

    wiped

---
+++ examples/process_to_dump.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+if __name__ == '__main__':
+    from os import getpid
+
+    print(f'pid {getpid()}\n')
+    del getpid
+
+    i = 0
+
+    from time import sleep
+    while i < 100:
+        i += 1
+        print(i, end=' ', flush=True)
+        sleep(1)
