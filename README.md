b2adc3f52e398fa94974108f552bee3e116fd0ad
AuthorName <Author@email.com>
2025-09-20 07:11:51 +0100

    wiped

---
+++ README.md
@@ -0,0 +1,70 @@
+> This project is an independent effort and is not affiliated with, sponsored by, or endorsed by CRIU (Checkpoint/Restore In Userspace).
+> CRIU is licensed under GPLv2.
+
+> Ubuntu x86_64 is recommended, linux system is requried.
+
+## Install
+
+```bash
+# source env.sh
+pip install git+https://github.com/herheliuk/criu-python-api
+```
+
+## Usage
+
+main terminal
+```bash
+# source env.sh
+python examples/example_usage.py
+```
+
+secondary terminal
+```bash
+tmux new-session 'python3 examples/process_to_dump.py'
+```
+
+## API Reference
+
+Overrides the CRIU dumps directory. (optional)
+```python
+criu_api.set_dumps_dir("/tmp/criu_api_dumps")
+```
+
+Removes the dumps directory and all its contents.
+```python
+criu_api.clean_up()
+```
+
+Sets the target process ID.
+```python
+criu_api.set_pid(1240)
+```
+
+Creates a full dump, or an incremental dump if memory tracking is available.
+```python
+criu_api.dump(i, leave_running=True, allow_overwrite=False)
+```
+
+Restores the specified dump into the current terminal session.
+```python
+criu_api.restore(24, kill_if_exists=False)
+```
+
+## Install CRIU
+
+```bash
+sudo apt install build-essential git pkg-config \
+    libprotobuf-dev libprotobuf-c-dev protobuf-c-compiler \
+    protobuf-compiler python3-protobuf uuid-dev \
+    libbsd-dev libcap-dev libnl-3-dev libnet1-dev \
+    libaio-dev libgnutls28-dev libdrm-dev iproute2 \
+    libnftables-dev asciidoc xmlto
+
+git clone https://github.com/checkpoint-restore/criu.git criu
+cd criu
+
+sudo make install
+
+cd ..
+rm -rf criu/
+```
---
+++ criu_api/__init__.py
@@ -0,0 +1,166 @@
+#!/usr/bin/env python3
+
+from os import geteuid, makedirs
+from sys import exit, executable, argv
+from platform import system
+from subprocess import Popen, run, PIPE
+
+if system() != "Linux":
+    raise RuntimeError('Unsupported operating system.')
+
+if geteuid() != 0:
+    run(['sudo', executable] + argv)
+    exit()
+
+del system, geteuid, executable, argv
+
+from pathlib import Path
+from re import escape
+from shutil import rmtree
+from psutil import pid_exists, Process
+
+class CRIUError(Exception):
+    """Custom exception for CRIU-related errors."""
+
+def _criu(args: list):
+    """Executes a CRIU command with the given arguments."""
+    process = None
+    try:
+        process = Popen(['criu'] + args, stdout=PIPE, stderr=PIPE, text=True)
+        stdout, stderr = process.communicate()
+        if process.returncode != 0:
+            raise CRIUError(f"criu {' '.join(args)}\n{stderr}{stdout}".strip())
+        return stdout.rstrip('\n')
+    except KeyboardInterrupt:
+        process.terminate()
+        raise
+
+_track_mem = False
+
+def _health_check(args_to_check: list[str] = []):
+    '''Verifies that CRIU is available and sets memory tracking.'''
+    global _track_mem
+    try:
+        result = _criu(['check', '--track-mem'])
+        _track_mem = True
+        return result
+    except:
+        _track_mem = False
+    return _criu(['check'] + args_to_check)
+
+_health_check()
+
+_pid: int = None
+
+def set_pid(pid: int):
+    '''Sets the target process ID.'''
+    global _pid
+    _pid = pid
+
+_dumps_dir: Path = None
+
+def set_dumps_dir(dumps_dir: str | Path = 'criu_dumps/'):
+    '''Overrides the CRIU dumps directory.'''
+    global _dumps_dir
+    if isinstance(dumps_dir, str):
+        _dumps_dir = Path(dumps_dir).resolve()
+    elif isinstance(dumps_dir, Path):
+        _dumps_dir = dumps_dir.resolve()
+    else:
+        raise TypeError(f"dumps_dir must be str or Path, not {type(dumps_dir).__name__} ({dumps_dir})!")
+
+set_dumps_dir()
+
+def clean_up(images_dir: Path = None):
+    '''Removes the dump or dumps directory and all its contents.'''
+    target_dir = images_dir or _dumps_dir
+    if target_dir.exists():
+        try:
+            rmtree(target_dir)
+        except PermissionError:
+            result = run(f"mount | grep -E '{escape(str(target_dir))}/.*/\\.criu\\.cgyard'", shell=True, capture_output=True, text=True)
+            criu_mounts = [line.split()[2] for line in result.stdout.strip().splitlines()]
+            for mnt in reversed(criu_mounts):
+                run(["umount", "-l", mnt], check=True)
+            rmtree(target_dir)
+
+_last_dump_number = -1
+
+def _remove_dumps_from(dump_number: int):
+    '''Removes the specified dump and all dumps after it'''
+    for _dump_number in range(dump_number, _last_dump_number + 1):
+        clean_up(_dumps_dir / str(_dump_number))
+
+shell_job = True
+tcp_established = True
+
+def _no_pid_warning():
+    return f'No pid was provided; Use {__name__}.set_pid().'
+
+def dump(dump_number: int = None, leave_running = True, allow_overwrite = False, additional_args: list[str] = None):
+    '''Creates a full dump, or an incremental dump if memory tracking is available.'''
+    if not _pid:
+        raise Exception(_no_pid_warning())
+    global _last_dump_number
+    not_overwritten = True
+    if dump_number:
+        if not isinstance(dump_number, int) or dump_number < 0:
+            raise ValueError(f'dump_number ({repr(dump_number)}) should be int >= 0.')
+        if dump_number > _last_dump_number + 1:
+            raise ValueError(
+                f'dump_number ({dump_number}) must be <= {_last_dump_number + 1}.'
+            )
+        if dump_number <= _last_dump_number:
+            if allow_overwrite:
+                _remove_dumps_from(dump_number)
+                not_overwritten = False
+            else:
+                raise FileExistsError(f'Dump {dump_number} already exists! Consider using allow_overwrite=True.')
+    else:
+        dump_number = _last_dump_number + 1
+    images_dir = f'{_dumps_dir}/{dump_number}'
+    makedirs(images_dir, exist_ok=allow_overwrite)
+    args = [
+        'dump',
+        f'--images-dir={images_dir}',
+        f'--tree={_pid}',
+    ]
+    if leave_running:
+        args.append('--leave-running')
+    if shell_job:
+        args.append('--shell-job')
+    if tcp_established:
+        args.append('--tcp-established')
+    if _track_mem and dump_number > 0 and not_overwritten:
+        args.extend([
+            f'--prev-images-dir=../{dump_number - 1}',
+            '--track-mem'
+        ])
+    if additional_args:
+        args.extend(additional_args)
+    return_value = _criu(args)
+    _last_dump_number = dump_number
+    return return_value
+
+def restore(dump_number: int = None, kill_if_exists = False, additional_args: list[str] = None):
+    '''Restores the specified dump into the current terminal session.'''
+    if kill_if_exists:
+        if not _pid:
+            raise Exception(_no_pid_warning())
+        elif pid_exists(_pid):
+            Process(_pid).kill()
+    if not dump_number:
+        dump_number = _last_dump_number
+    args = [
+        'restore',
+        f'--images-dir={_dumps_dir}/{dump_number}',
+    ]
+    if shell_job:
+        args.append('--shell-job')
+    if tcp_established:
+        args.append('--tcp-established')
+    if _track_mem:
+        args.append('--track-mem')
+    if additional_args:
+        args.extend(additional_args)
+    return _criu(args)
